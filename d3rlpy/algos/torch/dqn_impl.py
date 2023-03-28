b507f03504a2fb97d53948e03aa45f7741e8becf
Nina Kaploukhaya <49787999+NinaOwl@users.noreply.github.com>
2023-03-28 21:54:35 +0200

    Not joke

--- d3rlpy/algos/torch/dqn_impl.py
+++ d3rlpy/algos/torch/dqn_impl.py
@@ -19,8 +19,8 @@ from .utility import DiscreteQFunctionMixin
 from scipy.stats import bernoulli
 
 class DQNImpl(DiscreteQFunctionMixin, TorchImplBase):
-    _k: int
-    _allowed_actions: list
+   # _k: int
+    #_allowed_actions: list
     _learning_rate: float
     _optim_factory: OptimizerFactory
     _encoder_factory: EncoderFactory
@@ -34,8 +34,8 @@ class DQNImpl(DiscreteQFunctionMixin, TorchImplBase):
 
     def __init__(
         self,
-        k: int,
-        allowed_actions: list,
+        #k: int,
+        #allowed_actions: list,
         observation_shape: Sequence[int],
         action_size: int,
         learning_rate: float,
@@ -55,8 +55,8 @@ class DQNImpl(DiscreteQFunctionMixin, TorchImplBase):
             action_scaler=None,
             reward_scaler=reward_scaler,
         )
-        self._k = k
-        self._allowed_actions = allowed_actions
+        #self._k = k
+        #self._allowed_actions = allowed_actions
         self._learning_rate = learning_rate
         self._optim_factory = optim_factory
         self._encoder_factory = encoder_factory
@@ -137,13 +137,14 @@ class DQNImpl(DiscreteQFunctionMixin, TorchImplBase):
             next_actions = self._targ_q_func(batch.next_observations)
             bern_pr=bernoulli.rvs(p=.2, size=1)
             max_actions = next_actions.argsort(axis=1)
-            t = 0
-            max_action = []
+            allowed_actions = [ 2, 42, 22, 26, 18, 16, 33, 21, 25,  1, 14,  9, 15, 20,  4, 38,  8,
+       43, 36, 44, 12, 29,  7, 32, 23, 39, 27,  3, 37, 31,  6, 35, 17, 40,
+       30, 13, 10, 19, 34,  5, 11, 24, 28, 41]
             for i in range(len(max_actions)):
-                if t < self._k:
-                    if max_actions[len(max_actions) - 1 - i] in self._allowed_actions:
-                        max_action.append(max_actions[len(max_actions) - 1 - i])
-                        t = t + 1
+                if max_actions[len(max_actions) - 1 - i] in allowed_actions:
+                    max_action = max_actions[len(max_actions) - 1 - i]
+                    t = t + 1
+                    break
             return self._targ_q_func.compute_target(
                 batch.next_observations,
                 max_action,
